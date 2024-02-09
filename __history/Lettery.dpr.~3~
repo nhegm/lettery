03ff8c660eaa4ed92e1c56e67f6e016d3c533dad
nhegm <129392699+nhegm@users.noreply.github.com>
2024-02-09 18:49:52 +0800

    initialD (Windows + Android + iOS-alpha)

---
+++ __history/Lettery.dpr.~3~
@@ -0,0 +1,25 @@
+program Lettery;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  HeaderFooterTemplate in 'HeaderFooterTemplate.pas' {MainForm},
+  languageForm in 'languageForm.pas' {language},
+  meaningForm in 'meaningForm.pas' {meaningForm1},
+  statistics in 'statistics.pas' {statsForm},
+  themeForm in 'themeForm.pas' {setForm},
+  infoForm in 'infoForm.pas' {informationForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.FormFactor.Orientations := [TFormOrientation.Portrait];
+  Application.CreateForm(TMainForm, MainForm);
+  Application.CreateForm(Tlanguage, language);
+  Application.CreateForm(TmeaningForm1, meaningForm1);
+  Application.CreateForm(TstatsForm, statsForm);
+  Application.CreateForm(TsetForm, setForm);
+  Application.CreateForm(TinformationForm, informationForm);
+  Application.Run;
+end.
---
+++ meaningForm.pas
@@ -0,0 +1,77 @@
+unit meaningForm;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, System.IOUtils, FMX.StdCtrls, FMX.Objects,
+  FMX.Layouts, Math, System.Actions, FMX.ActnList, FMX.StdActns, FMX.Gestures,
+  System.Sensors, System.Sensors.Components, FMX.Memo.Types, FMX.ScrollBox,
+  FMX.Memo, IdIntercept, IdBaseComponent, IdLogBase, IdLogEvent,
+  FireDAC.Phys.Intf, FireDAC.Stan.Option, FireDAC.Stan.Intf, FireDAC.Comp.Client;
+
+type
+  TmeaningForm1 = class(TForm)
+    meaningText: TLabel;
+    ScrollBar: TScrollBar;
+
+    procedure FormShow(Sender: TObject);
+    procedure ScrollBarChange(Sender: TObject);
+
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  meaningForm1: TmeaningForm1;
+
+implementation
+
+uses HeaderFooterTemplate;
+
+{$R *.fmx}
+{$R *.LgXhdpiPh.fmx ANDROID}
+
+const
+  posY = 100;
+var
+  meaningString : String;
+
+procedure TmeaningForm1.FormShow(Sender: TObject);
+
+begin
+
+  Fill.Color := HeaderFooterTemplate.bckgrndColor[ColorsSetNumber];
+  meaningText.TextSettings.FontColor := HeaderFooterTemplate.boardNKeyTextColorsDef[ColorsSetNumber];
+  meaningForm1.Top:=50;
+  meaningForm1.Left:=70;
+  meaningForm1.Width:=round(meaningText.Width)+40;
+  meaningString := HeaderFooterTemplate.meaningOfTheWord;
+  meaningText.Text := AnsiToUTF8 (meaningString);
+
+  if meaningText.height+12<400
+    then begin
+      meaningText.Position.Y := 0;
+      meaningForm1.ScrollBar.Visible := false;
+      meaningForm1.height:=round(meaningText.height) + 8;
+    end
+    else begin
+      meaningForm1.height:=400;
+      meaningForm1.ScrollBar.Visible:=true;
+      ScrollBar.Min := 0;
+      ScrollBar.Value := 0;
+      ScrollBar.Max := meaningText.Height - meaningForm1.height + 8;
+    end;
+  meaningForm1.Active := true;
+  MainForm.Active := false;
+end;
+
+procedure TmeaningForm1.ScrollBarChange(Sender: TObject);
+begin
+  meaningText.Position.Y := - ScrollBar.Value;
+end;
+
+end.
